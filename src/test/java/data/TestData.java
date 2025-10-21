3dfd11ab8a0fab6d5350811387401439e69b02ae
Terekhov Artur <terehov.rtr@gmail.com>
2025-10-21 22:16:08 +0300

    added testData

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -13,11 +13,16 @@ public class TestData {
             number = faker.phoneNumber().subscriberNumber(10),
             notValidMinNumber = faker.phoneNumber().subscriberNumber(8),
             notValidMaxNumber = faker.phoneNumber().subscriberNumber(16),
+            day = String.valueOf(faker.number().numberBetween(1, 29)),
+            month = faker.options().option("January", "June", "August", "December"),
+            year = String.valueOf(faker.number().numberBetween(1900, 2100)),
             subject = faker.options().option("English", "Chemistry", "Computer Science", "Commerce", "Economics", "Social Studies"),
             hobbies = faker.options().option("Sports", "Reading", "Music"),
+            attachment = faker.options().option("Locators_table.pdf", "2. Git. GitHub.pdf"),
             address = faker.address().fullAddress(),
             state = faker.options().option("NCR","Uttar Pradesh","Haryana","Rajasthan"),
             city = TestData.chooseLocation(state);
+
 
     public static String chooseLocation(String state) {
 
